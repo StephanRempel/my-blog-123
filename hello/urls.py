833d250347f6a2fc156e42331601889f82934594
Stephan Rempel <28607147+StephanRempel@users.noreply.github.com>
2023-06-05 08:08:52 +0200

    polls path added

--- hello/urls.py
+++ hello/urls.py
@@ -4,7 +4,9 @@ from hello import views
 from hello.models import LogMessage
 
 home_list_view = views.HomeListView.as_view(
-    queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
+    queryset=LogMessage.objects.order_by("-log_date")[
+        :5
+    ],  # :5 limits the results to the five most recent
     context_object_name="message_list",
     template_name="hello/home.html",
 )
@@ -15,4 +17,5 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("log/", views.log_message, name="log"),
+    path("polls/", views.polls_index, name="polls"),
 ]
