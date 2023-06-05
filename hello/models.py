2e05773adc14c727e1837281dc27f4d0bf9077dc
Stephan Rempel <28607147+StephanRempel@users.noreply.github.com>
2023-06-05 07:54:02 +0200

    Question, Choice added

--- hello/models.py
+++ hello/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class LogMessage(models.Model):
     message = models.CharField(max_length=300)
     log_date = models.DateTimeField("date logged")
@@ -9,3 +10,22 @@ class LogMessage(models.Model):
         """Returns a string representation of a message."""
         date = timezone.localtime(self.log_date)
         return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        """Returns a string representation of a question."""
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """Returns a string representation of a choice."""
+        return self.choice_text
