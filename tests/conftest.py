6297be56848577b901eb6e8722fda59a80c2392a
Stephan Rempel <28607147+StephanRempel@users.noreply.github.com>
2023-06-11 22:37:16 +0200

    unused fixtures commented

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,19 +8,19 @@ def user_data():
     return {"email": "user_email", "name": "user_name", "password": "user_pass543"}
 
 
-@pytest.fixture
-def create_test_user(user_data):
-    user_model = get_user_model()
-    test_user = user_model.objects.create_user(**user_data)
-    test_user.set_password(user_data.get("password"))
-    return test_user
+# @pytest.fixture
+# def create_test_user(user_data):
+#     user_model = get_user_model()
+#     test_user = user_model.objects.create_user(**user_data)
+#     test_user.set_password(user_data.get("password"))
+#     return test_user
 
 
-@pytest.fixture
-def authenticated_user(client, user_data):
-    user_model = get_user_model()
-    test_user = user_model.objects.create_user(**user_data)
-    test_user.set_password(user_data.get("password"))
-    test_user.save()
-    client.login(**user_data)
-    return test_user
+# @pytest.fixture
+# def authenticated_user(client, user_data):
+#     user_model = get_user_model()
+#     test_user = user_model.objects.create_user(**user_data)
+#     test_user.set_password(user_data.get("password"))
+#     test_user.save()
+#     client.login(**user_data)
+#     return test_user
